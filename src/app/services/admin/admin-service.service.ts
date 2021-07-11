0f04d62f6d67057a705e9dfe2f6d331b505c954b
arpit-20 <arpitghai20@gmail.com>
2021-07-11 13:23:16 +0530

    service methods

--- src/app/services/admin/admin-service.service.ts
+++ src/app/services/admin/admin-service.service.ts
@@ -8,10 +8,29 @@ import { Observable } from 'rxjs';
 export class AdminService {
  private url = "http://localhost:7200/"
   constructor(private http: HttpClient) { }
+
   public adminLogin(data: any): Observable<any>{
     return this.http.post(this.url+"user/login", data);
   }
+
   public createAdmin(data: any): Observable<any>{
     return this.http.post(this.url+"user/register", data);
   }
+
+  public getAdminData(id?:any): Observable<any>{
+    if(id){
+    return this.http.get(this.url+"customer/"+id);
+    }else{
+
+    return this.http.get(this.url+"customer/");
+    }
+  }
+
+  public deleteAdminData(id:any): Observable<any>{
+    return this.http.delete(this.url+"customer/"+id); 
+}
+
+
+
+
 }
--- src/app/services/customer/customer-service.service.ts
+++ src/app/services/customer/customer-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private url = "http://localhost:7200/";
+
+  postCustomerData(data: any): Observable<any>{
+    return this.http.post(this.url+"customer/", data);
+  }
+  
 }
